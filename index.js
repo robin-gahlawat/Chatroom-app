e5ba82f991251d11824de29dc625dec69cdc1177
robin <robingahlawat30@gmail.com>
2022-04-03 12:20:53 +0530

    final commit

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ io.on('connection', socket=>{
 });
 
 
-const PORT = 3005 || process.env.PORT;
+const PORT = process.env.PORT || 3005;
 server.listen(PORT,'0.0.0.0',()=>{
     console.log('Server is up');
 });
